949186a5faed6a1067b599e0d54c5dc92e07c60f
DarinaStaevska <98123739+DarinaStaevska@users.noreply.github.com>
2022-05-06 19:17:26 +0300

    Initial_Commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Summator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            long sum = Summator.Sum(new int[] { 1, 2, 3, 4, 5, 6, });
+            if (sum == 21)
+            {
+                Console.WriteLine("TEST PASS");
+            } else
+            {
+                Console.WriteLine("TEST FAIL");
+            }
+            static void Main(string[] args)
+            {
+                long Average = Summator.Average(new int[] { 5, 10, 15 });
+                if (Average == 10)
+                {
+                    Console.WriteLine("TEST PASS");
+                }
+                else
+                {
+                    Console.WriteLine("TEST FAIL");
+                }
+            }
+        }
+
+
---
+++ Summator.cs
@@ -0,0 +1,26 @@
+﻿namespace Summator
+{
+    public static class Summator
+    {
+        public static long Sum(int[] arr)
+                {
+                long sum = 0;
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    sum += arr[i];
+                }
+                return sum;
+        }
+        public static int Average(int[] arr)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum/arr.Length;
+        }
+    }
+}
